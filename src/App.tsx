b759671239d93681f2d3ac861ec30bcac60e12a1
cundodev <facinditto@gmail.com>
2024-04-24 20:08:59 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='container m-auto min-h-screen'>
+      <main className='grid h-screen w-full place-content-center'>
+        <h1 className='text-2xl'>Some Gifs</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
