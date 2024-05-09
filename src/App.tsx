cc3fa1a92b436d94f55fa8aca8ec357950b4907e
cundodev <facinditto@gmail.com>
2024-05-09 16:08:08 -0300

    feat: create hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { HERO_GIF } from './utils/constants'
-import { Arrow } from './components/Icons'
+import Hero from './components/Hero'
 
 function App() {
   return (
@@ -7,22 +6,7 @@ function App() {
       <div className='absolute inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#be185d_100%)]'></div>
       {/* <header></header> */}
       <main className='flex h-screen w-full flex-col items-center overflow-auto'>
-        <section className='flex w-full flex-col items-center justify-between gap-4 rounded-lg p-6 text-center md:flex-row md:py-28 md:text-left'>
-          <div>
-            <h1 className='text-shadow mb-8 text-balance text-6xl font-bold'>
-              Some Gifs.
-            </h1>
-            <h2 className='text-balance text-2xl'>
-              Find your favorite gifs instantly.
-            </h2>
-          </div>
-          <div className='mt-auto rotate-[160deg]'>
-            <Arrow />
-          </div>
-          <picture className='hidden max-w-80 overflow-hidden rounded-lg md:block'>
-            <img className='object-contain' src={HERO_GIF} alt='hero image' />
-          </picture>
-        </section>
+        <Hero />
       </main>
       {/* <footer></footer> */}
     </div>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import { HERO_GIF } from '@/utils/constants'
+import { Arrow } from './Icons'
+
+export default function Hero() {
+  return (
+    <section className='flex w-full flex-col items-center justify-between gap-4 rounded-lg p-6 text-center md:flex-row md:py-28 md:text-left'>
+      <div>
+        <h1 className='text-shadow mb-8 text-balance text-6xl font-bold'>
+          Some Gifs.
+        </h1>
+        <h2 className='text-balance text-2xl'>
+          Find your favorite gifs instantly.
+        </h2>
+      </div>
+      <div className='mt-auto rotate-[160deg]'>
+        <Arrow />
+      </div>
+      <picture className='hidden max-w-80 overflow-hidden rounded-lg md:block'>
+        <img className='object-contain' src={HERO_GIF} alt='hero image' />
+      </picture>
+    </section>
+  )
+}
