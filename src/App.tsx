32b6df8958fb3e8e3e635288a1103a5f71cbdaa3
cundodev <facinditto@gmail.com>
2024-05-09 19:52:37 -0300

    fix: changed background and fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Hero from './components/Hero'
+import TrendingSearchTerms from './components/TrendingSearchTerms'
 
 function App() {
   return (
-    <div className='container m-auto max-w-5xl '>
-      <div className='absolute inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#be185d_100%)]'></div>
+    <div className='container m-auto max-w-5xl'>
+      <div className='absolute inset-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(255,46,137,0.3),rgba(255,255,255,0))]'></div>
       {/* <header></header> */}
-      <main className='flex h-screen w-full flex-col items-center overflow-auto'>
+      <main className='flex h-screen w-full flex-col items-center'>
         <Hero />
+        <TrendingSearchTerms />
       </main>
       {/* <footer></footer> */}
     </div>
