29dc260f7140e21c53147419e824a58e99ea133b
cundodev <facinditto@gmail.com>
2024-05-13 23:20:33 -0300

    fix margin scroll

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -6,15 +6,15 @@ export default function SearchForm({
   onSubmit: React.FormEventHandler
 }) {
   return (
-    <form onSubmit={onSubmit} className='flex w-full px-6 pb-6'>
+    <form onSubmit={onSubmit} className='flex w-full scroll-m-6 px-6 pb-6'>
       <input
         type='text'
         id='query'
         name='query'
         placeholder='Happy, Anime, The Avengers,...'
-        className='w-full rounded-l-lg bg-white bg-opacity-10 p-2'
+        className=' w-full rounded-l-lg bg-white bg-opacity-10 p-2'
       />
-      <button className='rounded-r-lg bg-fuchsia-600 p-2 text-center'>
+      <button className=' rounded-r-lg bg-fuchsia-600 p-2 text-center'>
         <SearchIcon />
       </button>
     </form>
