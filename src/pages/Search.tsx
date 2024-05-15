d5772e855488aae5fe8353940aaaa3c3096e7409
cundodev <facinditto@gmail.com>
2024-05-15 00:31:23 -0300

    feat: add return to home

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -5,8 +5,16 @@ import useGifs from '@/hooks/useGifs'
 export default function Search() {
   const { gifs } = useGifs()
   return (
-    <main className='mx-auto flex max-w-5xl flex-col items-center p-6'>
-      <SearchForm />
+    <main className='flex w-full max-w-5xl flex-col items-center gap-8 p-6'>
+      <section className='flex w-full flex-col gap-6 text-center md:flex-row md:items-center md:justify-between'>
+        <a
+          href='/'
+          className='text-shadow text-nowrap text-2xl font-bold hover:scale-105 '
+        >
+          Some Gifs.
+        </a>
+        <SearchForm />
+      </section>
       <Gifs gifs={gifs} />
     </main>
   )
