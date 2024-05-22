5b709d719f045e9522e312780c89a788f9b63a99
cundodev <facinditto@gmail.com>
2024-05-21 23:33:44 -0300

    feat: add functionality to add favorite gifs

--- src/components/Gifs.tsx
+++ src/components/Gifs.tsx
@@ -1,20 +1,47 @@
-import { Gif } from '@/types'
+import type { Gif } from '@/types'
+import { Heart, HeartFill } from './Icons'
+import { useState } from 'react'
 
 export default function Gifs({ gifs }: { gifs: Gif[] }) {
+  const [favorites, setFavorites] = useState<Map<Gif['id'], Gif>>(
+    () => new Map(),
+  )
+
+  function handleClick(currentGif: Gif) {
+    const draft = structuredClone(favorites)
+
+    if (draft.has(currentGif.id)) {
+      draft.delete(currentGif.id)
+    } else {
+      draft.set(currentGif.id, currentGif)
+    }
+    setFavorites(draft)
+  }
+
   return (
     <section className='h-full w-full columns-auto gap-2 sm:columns-2 md:columns-3'>
-      {gifs.map((gif) => (
-        <picture key={gif.id}>
-          <img
-            src={gif.webp}
-            alt={gif.title}
-            height={gif.height}
-            width={gif.width}
-            className='mb-2 w-full rounded-lg bg-slate-600'
-            loading='lazy'
-          />
-        </picture>
-      ))}
+      <ul>
+        {gifs.map((gif) => (
+          <li className='relative'>
+            <picture key={gif.id}>
+              <img
+                src={gif.webp}
+                alt={gif.title}
+                height={gif.height}
+                width={gif.width}
+                className='mb-2 w-full rounded-lg bg-slate-600'
+                loading='lazy'
+              />
+            </picture>
+            <button
+              className='absolute right-2 top-2 z-10 rounded-full bg-black/20 p-2 hover:text-red-500'
+              onClick={() => handleClick(gif)}
+            >
+              {favorites.has(gif.id) ? <HeartFill /> : <Heart />}
+            </button>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
