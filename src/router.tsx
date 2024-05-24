3739b3b75a93c74f3f071262536262b3b2dfe0b9
cundodev <facinditto@gmail.com>
2024-05-23 23:20:05 -0300

    feat: add favorite route

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Search from './pages/Search'
 import Layout from './layout'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: '/search',
         element: <Search />,
+      },
+      {
+        path: '/favorites',
+        element: <Favorites />,
       },
     ],
   },
