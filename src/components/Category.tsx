3112501566238770e9a41d212584b019a38c3fb8
cundodev <facinditto@gmail.com>
2024-05-28 23:34:51 -0300

    feat: add trends search terms marquee

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -6,7 +6,7 @@ export default function Category({ term }: { term: string }) {
   return (
     <Link
       to={`/search?q=${term}`}
-      className='text-nowrap rounded-lg border-2 p-2 text-center text-sm font-semibold hover:scale-110'
+      className='my-2 text-nowrap rounded-lg border-2 p-2 text-center text-sm font-semibold transition hover:scale-110 hover:!opacity-100 group-hover:opacity-50'
       style={{
         backgroundColor: `${randomColor.bg}0d`,
         borderColor: `${randomColor.border}`,
--- src/components/TrendingSearchTerms.tsx
+++ src/components/TrendingSearchTerms.tsx
@@ -1,5 +1,6 @@
 import { trendingTerms } from '@/utils/constants'
 import Category from './Category'
+import Marquee from 'react-fast-marquee'
 //import { useTrendingTerms } from '@/hooks/useTrendingTerms'
 
 export default function TrendingSearchTerms() {
@@ -8,15 +9,17 @@ export default function TrendingSearchTerms() {
   if (!trendingTerms) return null
 
   return (
-    <section className='max-w-5xl'>
-      <h2 className='mb-4 text-left text-xl font-semibold'>
-        Trending Search Terms
-      </h2>
-      <ul className='flex max-w-max flex-wrap gap-2'>
-        {trendingTerms.map((term, index) => (
-          <Category key={index + term} term={term} />
-        ))}
-      </ul>
+    <section className='group w-full max-w-5xl'>
+      <h2 className='mb-4 text-left text-xl font-semibold'>Trending Search Terms</h2>
+      <div>
+        <Marquee gradient gradientColor='#0a0a0a' gradientWidth={75} pauseOnHover speed={25} direction='right'>
+          <ul className='mr-4 flex max-w-max flex-wrap gap-4'>
+            {trendingTerms.map((term, index) => (
+              <Category key={index + term} term={term} />
+            ))}
+          </ul>
+        </Marquee>
+      </div>
     </section>
   )
 }
