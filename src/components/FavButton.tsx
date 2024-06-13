2bf260746fbba4287da3729db941fe8decbb4182
cundodev <facinditto@gmail.com>
2024-06-13 00:08:39 -0300

    feat: add login form popup on favorite action

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -1,16 +1,25 @@
 import useFavorites from '@/hooks/useFavorites'
 import { Heart, HeartFill } from './Icons'
 import type { Gif } from '@/types'
+import useAuth from '@/hooks/useAuth'
 
 export default function FavButton({ id }: { id: Gif['id'] }) {
   const { favsStorage, toggleFavorite } = useFavorites()
+  const { user } = useAuth()
 
-  return (
+  return user ? (
     <button
       className='absolute right-2 top-2 z-10 rounded-full bg-black/20 p-2 hover:text-red-500'
       onClick={() => toggleFavorite(id)}
     >
       {favsStorage.has(id) ? <HeartFill /> : <Heart />}
+    </button>
+  ) : (
+    <button
+      className='absolute right-2 top-2 z-10 rounded-full bg-black/20 p-2 hover:text-red-500'
+      popovertarget='signin'
+    >
+      <Heart />
     </button>
   )
 }
